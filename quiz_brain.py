b6d054acecb5ede22dc51f6941bbf5ae04ebc1a0
sinbum <sinbum@kakao.com>
2022-11-17 13:43:44 +0900

    my wrong_code in quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,9 @@
+def validate_answer(quiz_answer,user_answer):
+    if quiz_answer != user_answer:
+        print("your Answer is wrong")
+        return False
+
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -6,7 +12,11 @@ class QuizBrain:
     def next_question(self):
         quiz_number = self.question_number + 1
         quiz_question = self.question_list[self.question_number]
-        input(f"""Q.{quiz_number}: {quiz_question.text} (True/False) """)
+
+        for quiz in self.question_list:
+            answer = input(f"""Q.{quiz_number}: {quiz_question.text} (True/False) """)
+            if validate_answer(answer)
+            quiz_number += 1
 
 
     # def next_question(self):

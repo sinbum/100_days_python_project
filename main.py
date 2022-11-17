95ea8455f4f82f3315f3145c4b4abad4444a8a31
sinbum <sinbum@kakao.com>
2022-11-17 22:53:45 +0900

    quiz application is finished

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for data in question_data:
-    question = Question(data['text'], data['answer'])
+    question = Question(data['question'], data['correct_answer'])
     question_bank.append(question)
 
 quiz = QuizBrain(q_list=question_bank)
-quiz.next_question()
 
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+quiz.report_score()

a913f8f88d5bb42d052e01df39c0fea6cc82a582
sinbum <sinbum@kakao.com>
2022-11-21 18:40:57 +0900

    pandas를 사용해 csv 에서 dictionary 로 변경하기

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-import turtle
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
 import pandas
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
-guessed_states = []
+student_data_frame = pandas.DataFrame(student_dict)
 
-data = pandas.read_csv("50_states.csv")
-all_state = data.state.to_list()
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
 
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state")\
-        .title()
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
-    if answer_state == "Exit":
-        missing_states = [state for state in all_state if state not in guessed_states]
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
-        print(new_data)
-        break
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_alphabet_dic = {row.letter: row.code for (idx, row) in df.iterrows()}
+# print(nato_alphabet_dic)
 
-    if answer_state in all_state:
-        guessed_states.append(answer_state)
-        state_data = data[data["state"] == answer_state]
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_state)
+# 인풋을 받는다.
+# 대문자화 한다.
+# 한개의 단어를 한 글자 마다 nato의 밸류값으로 리스트에 저장하여 담는다.
+# 조건을 단다. 가지고있는 값이 있는지 없는지
+
+user_input = input("이름을 입력하세요").upper()
+user_input_lst = [nato_alphabet_dic[alphabet] for alphabet in user_input]
+
+# print(user_input_lst)
+
+# user_nato = {key: value for (key, value) in nato_alphabet_dic if key in user_input_lst}
+# print(user_nato)
 

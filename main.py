3bfbbadc979644b8f0ed464d0a6c179d5ddd4943
sinbum <sinbum@kakao.com>
2022-11-21 14:12:16 +0900

    make the game for united state map

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+import turtle
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+guessed_states = []
 
-print(data["temp"].max())
+data = pandas.read_csv("50_states.csv")
+all_state = data.state.to_list()
 
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state")\
+        .title()
 
-# data_dict =data.to_dict()
-# print(data_dict)
+    if answer_state in all_state:
+        guessed_states.append(answer_state)
+        state_data = data[data["state"] == answer_state]
 
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
-# temp_list = data["temp"].to_list
-# print(temp_list)
-
-
-
-#Create a dataframe from scratch
-
-data_dict = {
-    "students" : ["Amy","James","Angela"],
-    "scores" : [76,56,65]
-}
-df = pandas.DataFrame(data_dict)
-print(df.students)
-# df.to_csv("new_data.csv")
-
+screen.exitonclick()

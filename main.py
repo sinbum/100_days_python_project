13ae75e582ccbc84cd5905df4045af4155045215
sinbum <sinbum@kakao.com>
2022-11-21 16:40:36 +0900

    using list comprehensions

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state")\
         .title()
 
+    if answer_state == "Exit":
+        missing_states = [state for state in all_state if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        print(new_data)
+        break
+
     if answer_state in all_state:
         guessed_states.append(answer_state)
         state_data = data[data["state"] == answer_state]
@@ -25,4 +32,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
 
-screen.exitonclick()
